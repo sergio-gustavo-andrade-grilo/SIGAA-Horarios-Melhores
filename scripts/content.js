2e7bb4e0a7bcfff3f130b9b7aa332dd1c32e528a
sergio-gustavo-andrade-grilo <sergio.gustavo.andrade.grilo@ccc.ufcg.edu.br>
2024-11-20 19:54:06 -0300

    refactor: adiciona detecção para turmas com mais de 2 números nos horários e mais de 1 nos dias da semana

--- scripts/content.js
+++ scripts/content.js
@@ -1,126 +1,139 @@
-"use strict"
+"use strict";
 // TODO adicionar horários para CFP e CCJS
-// TODO funciona apenas para a homepage por enquanto...
 
-const expr = /^[1-7][MTN][1-5][1-5]$/g;
+const expr = /^[1-7]+[MTN][1-5]+$/g;
 
 const diasDaSemana = {
-    1: "dom",
-    2: "seg",
-    3: "ter",
-    4: "qua",
-    5: "qui",
-    6: "sex",
-    7: "sab"
+  1: "dom",
+  2: "seg",
+  3: "ter",
+  4: "qua",
+  5: "qui",
+  6: "sex",
+  7: "sab",
 };
 
 const horariosInicioManha = {
-    1: "07:00",
-    2: "08:00",
-    3: "09:00",
-    4: "10:10",
-    5: "11:10"
+  1: "07:00",
+  2: "08:00",
+  3: "09:00",
+  4: "10:10",
+  5: "11:10",
 };
 
 const horariosFimManha = {
-    1: "08:00",
-    2: "09:00",
-    3: "10:00",
-    4: "11:10",
-    5: "12:10"
+  1: "08:00",
+  2: "09:00",
+  3: "10:00",
+  4: "11:10",
+  5: "12:10",
 };
 
 const horariosInicioTarde = {
-    1: "13:00",
-    2: "14:00",
-    3: "15:00",
-    4: "16:10",
-    5: "17:10"
+  1: "13:00",
+  2: "14:00",
+  3: "15:00",
+  4: "16:10",
+  5: "17:10",
 };
 
 const horariosFimTarde = {
-    1: "14:00",
-    2: "15:00",
-    3: "16:00",
-    4: "17:10",
-    5: "18:10"
+  1: "14:00",
+  2: "15:00",
+  3: "16:00",
+  4: "17:10",
+  5: "18:10",
 };
 
 const horariosInicioNoite = {
-    1: "18:30",
-    2: "19:20",
-    3: "20:10",
-    4: "22:10",
+  1: "18:30",
+  2: "19:20",
+  3: "20:10",
+  4: "22:10",
 };
 
 const horariosFimNoite = {
-    1: "19:20",
-    2: "20:10",
-    3: "21:10",
-    4: "22:00",
+  1: "19:20",
+  2: "20:10",
+  3: "21:10",
+  4: "22:00",
 };
 
 function horarioInicio(turno, num) {
-    if (turno === "M") {
-        return horariosInicioManha[num]
-    } else if (turno === "T") {
-        return horariosInicioTarde[num]
-    } else {
-        return horariosInicioNoite[num]
-    };
-};
+  if (turno === "M") {
+    return horariosInicioManha[num];
+  } else if (turno === "T") {
+    return horariosInicioTarde[num];
+  } else {
+    return horariosInicioNoite[num];
+  }
+}
 
 function horarioFim(turno, num) {
-    if (turno === "M") {
-        return horariosFimManha[num]
-    } else if (turno === "T") {
-        return horariosFimTarde[num]
+  if (turno === "M") {
+    return horariosFimManha[num];
+  } else if (turno === "T") {
+    return horariosFimTarde[num];
+  } else {
+    return horariosFimNoite[num];
+  }
+}
+
+function diasString(dias) {
+  let out = "";
+
+  for (let dia of dias) {
+    out += diasDaSemana[dia] + " ";
+  }
+
+  return out.trim().split(" ").join(", ");
+}
+
+function converterHorarioParcial(horario) {
+  const turno = horario.match(/\D/)[0];
+  const horarioSplit = horario.split(turno);
+
+  const numsHorario = horarioSplit[1];
+
+  const inicio = parseInt(numsHorario[0]);
+  const fim = parseInt(numsHorario[numsHorario.length - 1]);
+
+  const out =
+    diasString(horarioSplit[0]) +
+    ": " +
+    horarioInicio(turno, inicio) +
+    "–" +
+    horarioFim(turno, fim);
+
+  return out.trim();
+}
+
+function converterHorarioCompleto(horario) {
+  let out = "";
+
+  for (let hor of horario.split(" ")) {
+    if (hor.match(expr)) {
+      out += converterHorarioParcial(hor) + " ";
     } else {
-        return horariosFimNoite[num]
-    };
-};
-
-function converterHorarioParcial(horarioParcial) {
-    const dia = parseInt(horarioParcial[0]);
-    const turno = horarioParcial[1];
-    const inicio = parseInt(horarioParcial[2]);
-    const fim = parseInt(horarioParcial[3]);
-
-    let out = "";
-
-    out += diasDaSemana[dia] + ": ";
-    out += horarioInicio(turno, inicio) + "–" + horarioFim(turno, fim);
-
-    return out;
-};
-
-function converterHorarioCompleto(horarioCompleto) {
-    let out = "";
-
-    for (let horario of horarioCompleto.split(" ")) {
-        if (horario.match(expr)) {
-            out += converterHorarioParcial(horario) + " ";
-        } else {
-            out += horario + " ";
-        }
+      out += hor + " ";
     }
+  }
 
-    return out;
-
-};
+  return out;
+}
 
 function converterHorarios() {
-    function traverseAndReplace(elemento) {
-        if (elemento.nodeType === Node.TEXT_NODE) {
-            elemento.nodeValue = converterHorarioCompleto(elemento.nodeValue);
-        }
-
-        for (let child of elemento.childNodes) {
-            traverseAndReplace(child);
-        }
+  function traverseAndReplace(elemento) {
+    if (elemento.nodeType === Node.TEXT_NODE) {
+      elemento.nodeValue = converterHorarioCompleto(elemento.nodeValue);
     }
 
-    traverseAndReplace(document.body);
+    for (let filho of elemento.childNodes) {
+      traverseAndReplace(filho);
+    }
+  }
+
+  traverseAndReplace(document.body);
 }
 
 converterHorarios();
